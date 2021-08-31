4f3db88c570c2dea97547c60038c8ef423e93ccb
Edison <edison16029@gmail.com>
2021-08-31 19:18:50 +0530

    Added Redux with Counter

--- src/Home/Home.screen.js
+++ src/Home/Home.screen.js
@@ -1,16 +1,37 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { decrement, increment } from './Home.slice';
 
 const HomeScreen = ({navigation}) => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <TouchableOpacity
-        style={{backgroundColor: 'red', height: '50%'}}
+        style={{backgroundColor: 'red', height: '10%'}}
         onPress={() => {
-          navigation.navigate('SinglePlayerGame');
+          console.log("Hello Eddie");
+          dispatch(increment());
+          // navigation.navigate('SinglePlayerGame');
         }}>
-        <Text>This is a placeholder for Home Screen</Text>
+        <Text>INCREMENT</Text>
       </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{backgroundColor: 'green', height: '10%'}}
+        onPress={() => {
+          console.log("Hello Eddie");
+          dispatch(decrement());
+          // navigation.navigate('SinglePlayerGame');
+        }}>
+        <Text>DECREMENT</Text>
+      </TouchableOpacity>
+
+      <Text>The counter value is {count}</Text>
+       
     </View>
   );
 };
